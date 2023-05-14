d06572be41e636810598bcf0ecc95c92f7f50e75
Tibe <fattahdede08@gmail.com>
2023-05-14 22:57:05 +0800

    ADD

---
+++ Konek.php
@@ -0,0 +1,14 @@
+<?php
+
+$host = 'localhost';
+$user = 'root';
+$pass = '';
+$db = 'project_web1';
+
+$koneksi = mysqli_connect($host, $user, $pass, $db);
+
+if(isset($koneksi)){
+    //echo"Koneksi berhasil";
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>My Website</title>
+    <link rel="stylesheet" href="ui.css">
+
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <script src="js/bootstrap.bundle.min.js"></script>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer"
+    />
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Glegoo:wght@400;700&display=swap" rel="stylesheet">
+
+</head>
+
+<body>
+
+    <nav class="navbar " style="background-color: #FEE037; height: 75px;">
+        <div class="container">
+            <form class="d-flex " role="search">
+                <input class="form-control me-1" type="search" placeholder="Search" aria-label="Search">
+                <button type="button" class="btn btn-light">
+                  <i class="fa-solid fa-magnifying-glass" style="color: #FEE037;"></i>
+                </button>
+
+            </form>
+            <ul class="nav justify-content-end fw-bold">
+                <li class="nav-item">
+                    <a class="nav-link active text-light" href="#">Home</a>
+                </li>
+                <li class="nav-item me-2">
+                    <a class="nav-link text-light" href="#">Sign in</a>
+                </li>
+                <li class="nav-item">
+                    <a href="login.php" type="button" class="btn btn-outline-light">
+                        Join
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+
+
+</html>
